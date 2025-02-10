118953dee17ca56d70d7961d2589d8f5bc6ae886
engrx0 <engr.log@gmail.com>
2025-02-10 12:44:48 -0800

    improved print border function

--- hello_engr.c
+++ hello_engr.c
@@ -1,61 +1,14 @@
 #include <ncurses.h>
 
-void print_border(int height, int width, int delay)
+void print_border(int start_row, int start_col, int height, int width, int delay);
+
+struct window_t
 {
-    addch(ACS_ULCORNER);
-    napms(delay);
-    refresh();
-
-    // draw top horizontal 
-    for (int i = 0; i < width - 1; i++)
-    {
-        addch(ACS_HLINE);
-        napms(delay);
-        refresh();
-    }
-
-    addch(ACS_URCORNER);
-    refresh();
-    napms(delay);
-
-    // draw right vertical 
-    for (int i = 1; i < height; i++)
-    {
-        move(i, width);
-        addch(ACS_VLINE);
-        refresh();
-        napms(delay);
-    }
-    
-    move(height, width);
-    addch(ACS_LRCORNER);
-    refresh();
-    napms(delay);
-
-    // draw bottom horizontal 
-    for (int i = width - 1; i > 0; i--)
-    {
-        move(height, i);
-        addch(ACS_HLINE);
-        refresh();
-        napms(delay);
-    }
-
-    move(height, 0);
-    addch(ACS_LLCORNER);
-    refresh();
-    napms(delay);
-
-    // draw left vertical 
-    for (int i = height - 1; i > 0; i--)
-    {
-        move(i, 0);
-        addch(ACS_VLINE);
-        refresh();
-        napms(delay);
-    }
-
-}
+    int xo;
+    int yo;
+    int width;
+    int height;
+};
 
 int main()
 {
@@ -66,11 +19,80 @@ int main()
 
     int main_height = 24;
     int main_width = 80;
-    print_border(main_height, main_width, 10);
+    print_border(0, 0, main_height, main_width, 10);
+    print_border(2, 10, 4, 60, 10);
+    print_border(8, 5, 15, 70, 10);
+
 
     getch();
 
     endwin(); // close ncurses
 
     return 0;
+}
+
+void print_border(int start_row, int start_col, int height, int width, int delay)
+{
+    int end_row = start_row + height;
+    int end_col = start_col + width;
+
+    // Draw top-left corner
+    move(start_row, start_col);
+    addch(ACS_ULCORNER);
+    napms(delay);
+    refresh();
+
+    // Draw top horizontal line
+    for (int col = start_col + 1; col < end_col; col++)
+    {
+        move(start_row, col);
+        addch(ACS_HLINE);
+        napms(delay);
+        refresh();
+    }
+
+    // Draw top-right corner
+    move(start_row, end_col);
+    addch(ACS_URCORNER);
+    napms(delay);
+    refresh();
+
+    // Draw right vertical line
+    for (int row = start_row + 1; row < end_row; row++)
+    {
+        move(row, end_col);
+        addch(ACS_VLINE);
+        napms(delay);
+        refresh();
+    }
+
+    // Draw bottom-right corner
+    move(end_row, end_col);
+    addch(ACS_LRCORNER);
+    napms(delay);
+    refresh();
+
+    // Draw bottom horizontal line
+    for (int col = end_col - 1; col > start_col; col--)
+    {
+        move(end_row, col);
+        addch(ACS_HLINE);
+        napms(delay);
+        refresh();
+    }
+
+    // Draw bottom-left corner
+    move(end_row, start_col);
+    addch(ACS_LLCORNER);
+    napms(delay);
+    refresh();
+
+    // Draw left vertical line
+    for (int row = end_row - 1; row > start_row; row--)
+    {
+        move(row, start_col);
+        addch(ACS_VLINE);
+        napms(delay);
+        refresh();
+    }
 }
